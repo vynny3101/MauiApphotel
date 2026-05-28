a90684e669e26d76278872c915e24d87d4353270
vinicius <santosvinicius9243@gmail.com>
2026-05-28 16:54:16 -0300

    add recusos ao app

--- MauiApphotel/App.xaml.cs
+++ MauiApphotel/App.xaml.cs
@@ -1,7 +1,37 @@
-﻿namespace MauiApphotel
+﻿using MauiApphotel.Models;
+
+namespace MauiApphotel
 {
     public partial class App : Application
     {
+        public List<Quarto> Lista_Quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao ="Suíte Super Luxo",
+                ValorDiariaadulto = 150.0,
+                ValorDiariaCrianca = 70.0
+            },
+             new Quarto()
+            {
+                Descricao ="Suíte Luxo",
+                ValorDiariaadulto = 110.0,
+                ValorDiariaCrianca = 50.0
+            },
+              new Quarto()
+            {
+                Descricao ="Suíte Solteiro",
+                ValorDiariaadulto = 100.0,
+                ValorDiariaCrianca = 45.0
+            },
+               new Quarto()
+            {
+                Descricao ="Suíte padrão",
+                ValorDiariaadulto = 70.0,
+                ValorDiariaCrianca = 30.0
+            }
+        };
+
         public App()
         {
             InitializeComponent();
--- MauiApphotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiApphotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,9 +2,22 @@ namespace MauiApphotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+
+	App PropriedadesApp; 
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.Lista_Quartos;
+
+		dtpck_checkin.MinimumDate = DateTime.Now;
+		dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+		dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+		dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(4);
+
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
@@ -17,5 +30,15 @@ public partial class ContratacaoHospedagem : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		DatePicker element = sender as DatePicker;
+
+		DateTime data_selecionada_checkin = element.Date;
+
+		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+		dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(4);
     }
 }
